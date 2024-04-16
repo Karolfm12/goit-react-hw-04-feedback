a4465156f17616615f1551228b1a5b9465fd8b7d
Karol Kotański <karolfm12@onet.eu>
2024-04-16 12:26:27 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Please leave feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+
+      <p>
+        Positive feedback:{' '}
+        {total > 0 && ((good / total) * 100).toFixed(0) + '%'}
+      </p>
+    </>
   );
 };
